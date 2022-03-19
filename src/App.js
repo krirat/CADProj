454920699f49175bd8862e3b2924ebc545911b52
krirat <krissadarattanasin@gmail.com>
2022-03-19 21:01:47 +0700

    Added hero section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './pics/logo.png';
+import heart from './pics/heart.png';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='header'>
+        <img src={logo} alt='logo'/>
+      </div>
+      <div className='container no-right-margin right-side'>
+            <div className='container-vert'>
+              <h1>Coronary Artery Disease</h1>
+              <p>Analyze your risk of getting a heart disease now!</p>
+            </div>
+            <img src={heart} alt='heart'/>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
