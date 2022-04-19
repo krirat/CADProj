9869ffefa257a6a08f4c0ac414acc29ef35bf03f
krirat <krissadarattanasin@gmail.com>
2022-04-20 01:09:00 +0700

    Added about section and made changes to css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './pics/logo.png';
 import heart from './pics/heart.png';
+import girl from './pics/girl.png';
 import './App.css';
+import Button from '@mui/material/Button';
 
 
 function App() {
   return (
-    <div className='app'>
-      <div className='header'>
-        <img src={logo} alt='logo'/>
-      </div>
-      <div className='container no-right-margin right-side'>
-            <div className='container-vert'>
-              <h1>Coronary Artery Disease</h1>
-              <p>Analyze your risk of getting a heart disease now!</p>
-            </div>
-            <img src={heart} alt='heart'/>
-      </div>
-      <div>
-
-      </div>
-    </div>
+    <>
+      <section className='hero'>
+        <div className='container container-hero'>
+              <div className='container container-vert container-vert-hero'>
+                <img className='logo' src={logo} alt='logo'/>
+                <h1>Coronary Artery Disease</h1>
+                <p>Analyze your risk of getting a heart disease now!</p>
+              </div>
+              <img className="heart-img" src={heart} alt='heart'/>
+        </div>
+      </section>
+      <section className='about-cad'>
+        <div className='container container-about'>
+          <div className='container container-vert add-right-margin'>
+            <h1>What is CAD?</h1>
+            <p>
+              Coronary Heart Disease is the most common form of heart disease
+              caused when coronary arteries fail to supply your heart with blood,
+              oxygen and nutrients. Cholesterol deposits and plaqyes are to blame
+              most of the time. The good news is you can lower your risk of getting
+              one by making lifestyle changes.
+            </p>
+            <Button variant="contained" size="large">TAKE A TEST</Button>
+          </div>
+          <img src={girl} alt='girl'/>
+        </div>
+      </section>
+    </>
   );
 }
 
