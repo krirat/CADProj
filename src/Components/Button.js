28be97dbe44b0fd057bb1135228c276b84fedd58
krirat <krissadaraguser.name>
2022-10-04 18:33:44 +0000

    fixed scaling issues

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import MUIButton from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-
-const types = {
-    small : 'small'
-}
+import React from "react";
+import { Link } from "react-router-dom";
+import MUIButton from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
 
 const Button = styled(MUIButton)(() => ({
-    color: '#F6F6F6',
-    backgroundColor: '#48695C',
-    fontFamily: 'Montserrat',
-    padding: '0.5rem 1.5rem',
-    fontSize: '0.7rem',
-    '&:hover' : {
-        backgroundColor: '#3B574C'
-    }
+  color: "#F6F6F6",
+  backgroundColor: "#48695C",
+  fontFamily: "Montserrat",
+  padding: "0.8rem 2.5rem",
+  margin: "2rem 0 4rem 0",
+  fontSize: "1.5rem",
+  borderRadius: "3rem",
+  "&:hover": {
+    backgroundColor: "#3B574C",
+  },
+  "@media (max-width:700px)": {
+    margin: "1rem 0 1.2rem 0",
+    alignSelf: "center",
+  },
 }));
 function styledButton(props) {
-    return (<Button variant="contained" size="large" disableRipple><Link style={{textDecoration:'none', color: '#F6F6F6'}} to={props.to}>{props.text}</Link></Button>)
+  return (
+    <Button variant="contained" size="large" disableRipple>
+      <Link style={{ textDecoration: "none", color: "#F6F6F6" }} to={props.to}>
+        {props.text}
+      </Link>
+    </Button>
+  );
 }
 
 export default styledButton;
